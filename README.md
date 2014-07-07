fc45c65833c671d42d2274a557dce59ab84cafdf
Maximilian Irro <max@disposia.org>
2014-07-07 18:10:45 +0200

    rewrote the code structure; added command line options

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 # daytweet
 
-daytweet is a Twitter Bot that informs you of (maybe unofficial or fictional) observances/holidays and awareness days
-
-You definitely should [follow it on Twitter](http://twitter.com/daytweet).
-
-This programs repository may be found on [github](http://github.com/mpgirro/daytweet).
+daytweet is a Twitter bot that informs you of (maybe unofficial or fictional) observances/holidays and awareness days. You definitely should [follow it on Twitter](http://twitter.com/daytweet). The source code is on [Github](http://github.com/mpgirro/daytweet).
 
 ## Database
 
-In order to make some dates easy to save it does not use an ordenary database. Instead the events are stored in an iCalendar (daytweet.ics) file. It makes life with stuff like "last friday in November" much easier. You can use the file and add it to your favourite calendar program if you want to look ahead what is comming next. Every date has a URL attached if found any useful. They are mainly Wikipedia articles.
+To make the database maintainence easy the events are stored in an iCal file. Instead the events are stored in an iCalendar file (daytweet.ics by default). It makes life with stuff like "last friday in November" much easier. You can use the file and add it to your favourite calendar program if you want to look ahead what is comming next. Every date has a URL attached if found any useful. They are mainly Wikipedia articles.
 
 ## Tweet Style
 
@@ -22,20 +18,20 @@ There are a few Ruby Gems that needs to be installed before daytweet can be run.
 The required gems are: twitter, shorturl, icalendar, parseconfig
 They may be installed with these commands:
 
-    sudo gem install icalendar
-    sudo gem install twitter
-    sudo gem install shorturl
-    sudo gem install parseconfig
+    gem install icalendar
+    gem install twitter
+    gem install optparse
+    gem install parseconfig
     
-## Configuration file
+## Configuration
 
-In order to post stuff in Twitter, an application needs consumer and oauth token/secrets. See [Twitter Developer](http://dev.twitter.com) for information on how you can get those.
+In order to post stuff on Twitter, an application needs consumer and oauth token/secrets. See [Twitter Developer](http://dev.twitter.com) for information on how you can get those.
 
-daytweet expects those information in a configuration file called daytweet.conf. See the provided example.conf on how this file should look like. The file is expected in the same directory as daytweet.rb. This can be changed by altering the constant CONFIG_FILE within daytweet.rb.
+daytweet expects those information in a configuration file called daytweet.conf. See the provided daytweet.conf.example on how this file should look like. The file is expected in the same directory as daytweet.rb. The CLI offers the use of a different file location.
 
 ## Operation
 
-    ./daytweet.rb
+    ./daytweet.rb [options]
     
-This will post all todays events on Twitter. Therefore this program must be run once a day, every day. It should not take more than a moment. In order to do this automatically, it is recommended to use a [cronjob](http://en.wikipedia.org/wiki/Cron) or a [runwhen entry](http://code.dogmap.org/runwhen/).
+This will post all todays events on Twitter. Therefore this program swhould be run once a day, every day. It should not take more than a moment. In order to do this automatically, it is recommended to use a [cronjob](http://en.wikipedia.org/wiki/Cron) or a [runwhen entry](http://code.dogmap.org/runwhen/).
 
--- daytweet.rb
+++ daytweet.rb
@@ -1,125 +1,104 @@
 #!/usr/bin/env ruby
 # -*- encoding : utf-8 -*-
 
-require 'rubygems' 
-require 'icalendar'
-require 'date'
-require 'twitter'
-#require 'shorturl'
-require 'parseconfig'
+# author: Maximilian Irro <max@disposia.org>, 2012-2014
 
-
-# location of config file
-CONFIG_FILE = "daytweet.conf" # you may want to change this
-
-
-# returns array of all events happening today
-# (= date of running program)
-def today_events
-
-    # open icalendar file
-    cal_file = nil
-    begin
-        cal_file = File.open(@ICAL_FILE, "r")
-    rescue
-        puts "Error trying to open ical file: #{$!.message}" 
-        return []
-    end
-    
-    # get the calendar object
-    cals = Icalendar.parse(cal_file)
-    cal = cals.first
-    
-    # make array of all the events that are today
-    events = []
-    cal.events.each do |event|
-        events << event if event.dtstart.to_s == DateTime.now.strftime("%Y-%m-%d")
-    end
-    
-    return events
+begin
+  require 'icalendar'
+  require 'date'
+  require 'twitter'
+  require 'optparse'
+  require 'parseconfig'
+rescue LoadError
+  puts "Some gems seem to be not installed. You need to do that first: #{$!.message}"
+  exit
 end
 
+DEFAULT_CONFIG_FILE = "daytweet.conf" 
+USAGE = "Usage: daytweet.rb [options]"
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = USAGE
+  opts.separator ""
+  opts.separator "Specific options:"
+
+  opts.on("-c", "--config FILE", "Use a different config file than the default #{DEFAULT_CONFIG_FILE}") do |config_file|
+    options[:config] = true
+    options[:config_file] = config_file
+  end
+
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end.parse! # do the parsing. do it now!
+
+
+options[:config_file] = DEFAULT_CONFIG_FILE unless options[:config]
 
 # reads the config params from the config file
-def parse_configs
+begin
+  configs = ParseConfig.new(options[:config_file])
+rescue
+  puts "error reading config file: #{options[:config_file]}"
+  exit
+end
 
-    config = ParseConfig.new(CONFIG_FILE)
+CONSUMER_KEY        = configs['consumer_key']
+CONSUMER_SECRET     = configs['consumer_secret']
+OAUTH_TOKEN         = configs['oauth_token']
+OAUTH_TOKEN_SECRET  = configs['oauth_token_secret']
+ICAL_FILE           = configs['ical_file']
+
+
+# open icalendar file
+cal_file = nil
+begin
+    cal_file = File.open(ICAL_FILE, "r")
+rescue
+    puts "Error trying to open ical file: #{$!.message}" 
+    exit
+end
+
+# get the calendar object
+cals = Icalendar.parse(cal_file)
+cal = cals.first
+
+today_date_str = DateTime.now.strftime("%Y-%m-%d")
+
+# make array of all the events that are today
+todays_events = []
+cal.events.each do |event|
+    todays_events << event if event.dtstart.to_s == today_date_str
+end
+
+
+unless todays_events.empty?
+  
+  Twitter.configure do |config|
+    config.consumer_key       = @CONSUMER_KEY
+    config.consumer_secret    = @CONSUMER_SECRET
+    config.oauth_token        = @OAUTH_TOKEN
+    config.oauth_token_secret = @OAUTH_TOKEN_SECRET
+  end
+  
+  todays_events.each do |event|
+    tweet_msg = compose_tweet(event)
     
-    @CONSUMER_KEY        = config['consumer_key']
-    @CONSUMER_SECRET     = config['consumer_secret']
-    @OAUTH_TOKEN         = config['oauth_token']
-    @OAUTH_TOKEN_SECRET  = config['oauth_token_secret']
-    @ICAL_FILE           = config['ical_file']
+    tweet_msg = "[#{today_date_str}] Today is #{event.summary}"
+    tweet_msg = tweet_msg + " • " + event.url.to_s if !event.url.nil? && (tweet_msg + " • " + event.url.to_s).length <= 140    
+    puts tweet_msg
+    #Twitter.update(tweet_msg)
+  end
+else  
+  puts "No events today"
 end
 
 
-# tweets msg using the keys and tokens from config file
-def tweet(msg)
-
-    Twitter.configure do |config|
-      config.consumer_key       = @CONSUMER_KEY
-      config.consumer_secret    = @CONSUMER_SECRET
-      config.oauth_token        = @OAUTH_TOKEN
-      config.oauth_token_secret = @OAUTH_TOKEN_SECRET
-    end
-
-    Twitter.update(msg)
-end
 
 
-# makes a message string fitting the 140 character limit
-# if an event has a url attached, it will be appended, if 
-# it does not supersize 140. if it does, it will be shortened.
-# if this fails, the url will not be appended
-def tweet_msg(event)
 
-    msg = "Today is " + event.summary
-    if event.url != nil
-        
-        url_msg = msg + " • " + event.url.to_s
-        
-        # if url makes tweet to long, try url shortening
-        if url_msg.length > 140
-            
-            url_msg = msg # remove this after fix!
-            
-            """ SyntaxError on LINUX systems with ShortURL gem
-            begin 
-                url_msg = msg + ' • ' + ShortURL.shorten(event.url, :tinyurl)
-            rescue  # the url shortener broke. again...
-                url_msg = msg
-            else # hey it worked
-                if url_msg.length > 140
-                    url_msg = msg # still too long
-                end
-            end
-            """
-        end
-        
-        msg = url_msg
-    end
-
-    return msg
-end
-
-
-# program entry point
-# - - - - - - - - - - 
-
-parse_configs
-
-events = today_events 
-
-# tweet every event of today
-if events.empty? == false
-    events.each do |e|
-        msg = tweet_msg(e)
-        puts msg
-        tweet(msg)
-    end
-else
-    puts "No events today"
-end
 
 
 
